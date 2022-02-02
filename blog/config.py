196463f47fa06708ce5097078d3e610b0ceb6525
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2022-02-02 13:50:16 -0500

    Changed db name

--- blog/config.py
+++ blog/config.py
@@ -4,6 +4,6 @@ import os
 def get_database_uri() -> str:
     host = os.environ.get("DB_HOST", "localhost")
     port = 5432
-    password = os.environ.get("DB_PASSWORD", "abc123")
+    password = os.environ.get("DB_PASSWORD", "123")
     user, db_name = "blog", "blog"
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
